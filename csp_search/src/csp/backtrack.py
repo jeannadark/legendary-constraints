74a12bbb61b3592e20f44eed7da4c8ccb35e85a0
Narmin Jamalova <65672621+njamalova@users.noreply.github.com>
2021-02-26 19:49:08 +0400

    minor update

--- csp_search/src/csp/backtrack.py
+++ csp_search/src/csp/backtrack.py
@@ -11,12 +11,12 @@ def backtrack_search(csp: Any, graph: Any) -> Any:
     """Perform backtrack search.
 
     This function will pick up the most constrained vertex from the list of unassigned vertices and
-    try assigning the least constraining color to it. After that, the algorithm will check if the
-    assignment is consistent (i.e. that it has not been assigned to any other vertex) and if yes, then
-    perform an arc consistency check upon which keep recursively backtracking until all vertices have colors
+    try assigning the least constraining color to it if the assignment is consistent 
+    (i.e. that it has not been assigned to any other vertex) and if yes, then
+    perform an arc consistency check upon which will keep recursively backtracking until all vertices have colors
     assigned to them.
 
-    If the color assignment is not consistent, then the given color is unassigned from the vertex.
+    If no solution found with the current color assignment, then it is de-assigned.
     
     :param csp: given csp problem
     :type csp: any
@@ -31,9 +31,9 @@ def backtrack_search(csp: Any, graph: Any) -> Any:
     next_vertex = get_most_constrained_vertex(graph=graph, csp=csp)
 
     for color in get_least_constraining_values(csp=csp, vertex=next_vertex):
-        csp.assign_color(v_id=next_vertex, color=color)
+        
         if is_consistent(csp=csp, v_id=next_vertex, color=color):
-            
+            csp.assign_color(v_id=next_vertex, color=color)
             if AC3(
                 csp=csp,
                 queue=[(Xk, next_vertex) for Xk in csp.constraints[next_vertex]],
@@ -42,6 +42,5 @@ def backtrack_search(csp: Any, graph: Any) -> Any:
             
                 if result:
                     return result
-
-        csp.unassign_color(v_id=next_vertex)
+            csp.unassign_color(v_id=next_vertex)
     return None
